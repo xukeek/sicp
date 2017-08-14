fc8edb6db2f33e967815cc99968152931c758e9d
xkk <okeoke1011@gmail.com>
2017-08-14 16:33:13 +0800

    2.18

--- two/hierarchical_data.scm
+++ two/hierarchical_data.scm
@@ -1,4 +1,13 @@
+;2.17
 (define (last-pair pair)
   (cond ((null? pair) '())
         ((null? (cdr pair)) (car pair))
         (else (last-pair (cdr pair)))))
+
+;2.18
+(define (reverse pair)
+  (define (go p r)
+    (if (null? p)
+        r
+        (go (cdr p) (cons (car p) r))))
+  (go pair '()))
