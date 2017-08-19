6e9234ec4da488c646addde71a04bcf2c425bbdb
xukeek <okeoke1011@gmail.com>
2017-08-19 12:42:17 +0800

    2.36,2.37,2.39

--- two/hierarchical_data.scm
+++ two/hierarchical_data.scm
@@ -69,3 +69,41 @@
           (if (pair? x)
               (count-leaves x)
               1)) t)))
+
+;2.36
+(define (accmulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accmulate op init (map car seqs))
+            (accmulate-n op init (map cdr seqs)))))
+
+;2.37
+(define (dot-product v w)
+  (accmulate + 0 (map * v w)))
+
+(define (matrix-*-verctor m v)
+  (map (lambda (mi) (dot-product mi v)) m))
+
+(define (transpose mat)
+  (accmulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (mi) (matrix-*-verctor cols mi)) m)))
+
+;2.39
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse sequence)
+  (accmulate (lambda (x y) (append y (list x))) '() sequence))
+
+(define (reverse2 sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
+
+(reverse (list 1 2 3 4))
